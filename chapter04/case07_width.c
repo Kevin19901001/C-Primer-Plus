3527909cddb0d06b169f94d1ff2f456791c92128
Kevin <huanqing19901001@163.com>
2024-02-28 12:50:11 +0800

    Update case07_width.c

--- chapter04/case07_width.c
+++ chapter04/case07_width.c
@@ -10,22 +10,22 @@
 int main(void)
 {
     printf("% d\n", PAGES);
-	printf("%#d\n", PAGES);
+    printf("%#d\n", PAGES);
     printf("%+d\n", PAGES);
-	printf("%-d\n", PAGES);
-	printf("%0d\n", PAGES);
+    printf("%-d\n", PAGES);
+    printf("%0d\n", PAGES);
 	
-	printf("\n");
-	printf("*%d*\n", PAGES);	// *959*
+    printf("\n");
+    printf("*%d*\n", PAGES);	// *959*
     printf("*%2d*\n", PAGES);	// *959*
     printf("*%10d*\n", PAGES);	// *       959*
     printf("*%-10d*\n", PAGES);	// *959       *
 
-	printf("%\n", PAGES);
+    printf("%\n", PAGES);
     printf("*%010d*\n", PAGES);
     printf("*%#10d*\n", PAGES);
-	printf("*% 10d*\n", PAGES);
-	printf("*%+10d*\n", PAGES);
+    printf("*% 10d*\n", PAGES);
+    printf("*%+10d*\n", PAGES);
 
     return 0;
 }
