2d62cdbeda67d8d4efd2a509f5d354f0b50bf119
Kevin <huanqing19901001@163.com>
2024-02-29 12:35:19 +0800

    Update case07_width.c

--- chapter04/case07_width.c
+++ chapter04/case07_width.c
@@ -1,21 +1,3 @@
-/*
- * 使用修饰符和标记
- *
- * width.c 字段宽度
- *
- */
-#include <stdio.h>
-#define PAGES 959
-
-int main(void)
-{
-    printf("% d\n", PAGES);
-    printf("%#d\n", PAGES);
-    printf("%+d\n", PAGES);
-    printf("%-d\n", PAGES);
-    printf("%0d\n", PAGES);
-	
-    printf("\n");
     printf("*%d*\n", PAGES);	// *959*
     printf("*%2d*\n", PAGES);	// *959*
     printf("*%10d*\n", PAGES);	// *       959*
@@ -26,6 +8,17 @@ int main(void)
     printf("*%#10d*\n", PAGES);
     printf("*% 10d*\n", PAGES);
     printf("*%+10d*\n", PAGES);
+	
+	// short int
+	printf("%6.4hd\n", SHORTINT);
+	printf("%3hd\n", SHORTINT);
+	
+	// signed char
+	printf("%6.4hhd\n", SIGNEDCHAR);
+	printf("%6hhd\n", SIGNEDCHAR);
+	printf("%hhd\n", SIGNEDCHAR);
+	printf("%4hhu\n", SIGNEDCHAR);
+	printf("%3hhx\n", SIGNEDCHAR);
 
     return 0;
 }
