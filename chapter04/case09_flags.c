cfb775b57535f05e69a9d2b3f0d1baaf4e6068d4
huanqing19901001@163.com <huanqing19901001@163.com>
2024-03-02 22:31:19 +0800

    Update case09_flags.c.

--- chapter04/case09_flags.c
+++ chapter04/case09_flags.c
@@ -7,7 +7,8 @@
 int main(void)
 {
 
-  printf("%x %X %#x\n", 31, 31, 31);    // 1f 1F 0x1f
+	printf("%x %X %#x\n", 31, 31, 31);    // 1f 1F 0x1f
+	printf("**%d**% d**% d**\n", 42, 42, -42);
 
-  return 0;
+	return 0;
 }
