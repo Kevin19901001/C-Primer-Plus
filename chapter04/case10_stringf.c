7618c11fd12c4100960fe63a7eb9c9438b2fb3de
huanqing19901001@163.com <huanqing19901001@163.com>
2024-03-04 20:58:44 +0800

    Update case10_stringf.c.

--- chapter04/case10_stringf.c
+++ chapter04/case10_stringf.c
@@ -1,5 +1,8 @@
 /*
  * stringf.c -- 字符串格式
+ * 
+ * Date: 2024-03-04
+ * Author: FanHuanQing
  */
 #include <stdio.h>
 #define BLURB "Authentic imitation"
@@ -7,10 +10,10 @@
 int main()
 {
 	//
-	printf("[%2s]\n", BLURB);    // [Authentic imitation]
-	printf("[%24s]\n", BLURB);    // [     Authentic imitation]
-	printf("[%24.5s]\n", BLURB);    // [                   Authe]
-	printf("[%-24.5s]\n", BLURB);    // [Authe                   ]
+	printf("[%2s]\n", BLURB);	// [Authentic imitation]
+	printf("[%24s]\n", BLURB);	// [     Authentic imitation]
+	printf("[%24.5s]\n", BLURB);	// [                   Authe]
+	printf("[%-24.5s]\n", BLURB);	// [Authe                   ]
    
-   return 0;
+	return 0;
 }
