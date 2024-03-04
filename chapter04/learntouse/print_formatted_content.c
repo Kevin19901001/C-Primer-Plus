fad916373eed6bf9b7f8dfca63b55e509ca5d66b
huanqing19901001@163.com <huanqing19901001@163.com>
2024-03-04 20:55:40 +0800

    Create print_formatted_content.c.

---
+++ chapter04/learntouse/print_formatted_content.c
@@ -0,0 +1,17 @@
+/*
+ * 学以致用
+ * 2024-03-04
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char name[40] = "Kevin";
+	float cash = 102.32;
+
+	printf("The %s family just maybe $%5.2f dollars richer!\n", name, cash);
+
+	return 0;
+}
+
