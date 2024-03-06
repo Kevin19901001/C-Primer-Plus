1388cb67c13087a02af72f1b922abe88881c2afb
Kevin <huanqing19901001@163.com>
2024-03-06 12:20:20 +0800

    Update case15_input.c

--- chapter04/case15_input.c
+++ chapter04/case15_input.c
@@ -8,5 +8,14 @@
 #include <stdio.h>
 int main(void)
 {
+        int age;
+        float assets;
+        char pet[30];
+
+        printf("Enter your age, assets and favorate pet:\n");
+        scanf("%d %f", &age, &assets);
+        scanf("%s", pet);
+        printf("%d %f %s\n", age, assets, pet);
+
         return 0;
 }
