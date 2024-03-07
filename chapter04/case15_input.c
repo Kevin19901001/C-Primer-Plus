80058326f9194693caa5fab43b33d97cfdf2c434
Kevin <huanqing19901001@163.com>
2024-03-07 12:21:57 +0800

    Update case15_input.c

--- chapter04/case15_input.c
+++ chapter04/case15_input.c
@@ -17,26 +17,26 @@ int main(void)
         scanf("%s", pet);
         printf("%d %f %s\n", age, assets, pet);
 
-	// 把输入解释成有符号十进制整数
-	int num1;
-	int num2;
-	printf("Please enter integer values:\n");
-	scanf("%d %i", &num1, &num2);
-	printf("num1:%d, num2:%i\n", num1, num2);
+	    // 把输入解释成有符号十进制整数
+	    int num1;
+	    int num2;
+	    printf("Please enter integer values:\n");
+	    scanf("%d %i", &num1, &num2);
+	    printf("num1:%d, num2:%i\n", num1, num2);
 
-    // %e、%f、%g、%a 把输入解释称浮点数
-    float fltvale;
-    float fltvalf;
-    float fltvalg;
-    float flrvala;
-    printf(“Please enter float values:\n”);
-    scanf(“%e %f %g %a”, &fltvale, &fltvalf, &fltvalg, &fltvala);
+        // %e、%f、%g、%a 把输入解释称浮点数
+        float fltvale;
+        float fltvalf;
+        float fltvalg;
+        float flrvala;
+        printf(“Please enter float values:\n”);
+        scanf(“%e %f %g %a”, &fltvale, &fltvalf, &fltvalg, &fltvala);
 
-	// %c 把输入解释成字符串
-	char chrval;
-	printf("Please enter a char value:\n");
-	scanf("%c", &chrval);
-	printf("The char value is %c\n", chrval);
+	    // %c 把输入解释成字符串
+	    char chrval;
+	    printf("Please enter a char value:\n");
+	    scanf("%c", &chrval);
+	    printf("The char value is %c\n", chrval);
 
-        return 0;
+       return 0;
 }
