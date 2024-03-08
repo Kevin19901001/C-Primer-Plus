2ade7b4b352fc7af20422299ddc4c69c2afa1e53
huanqing19901001@163.com <huanqing19901001@163.com>
2024-03-08 20:49:21 +0800

    Update case15_input.c

--- chapter04/case15_input.c
+++ chapter04/case15_input.c
@@ -42,7 +42,28 @@ int main(void)
         scanf("%E %F %G %A", &fltvalbe, &fltvalbf, &fltvalbg, &fltvalba);
         printf("fltvalbe=%E, fltvalbf=%F, fltvalbg=%G, fltvalba=%A\n", fltvalbe, fltvalbf, fltvalbg, fltvalba);
 
-	// %c 把输入解释成字符串
+	// %o 把输入解释成有符号八进制整数
+	int octval;
+	printf("Please enter a octonary value:\n");
+	scanf("%o", &octval);
+	printf("The octonary value you have input in integer is: %d\n", octval);
+
+	// %p 把输入解释成指针（地址）
+	
+
+	// 把输入解释成字符串
+	
+
+	// %u 把输入解释成无符号十进制整数
+	unsigned int unsgnval;
+	printf("Please enter a unsigned integer:\n");
+	scanf("%u", &unsgnval);
+	printf("unsgnval = %u\n", unsgnval);
+
+	// %x %X 把输入解释成有符号十六进制整数
+	
+
+	// %c 把输入解释成字符
 	char chrval;
 	printf("Please enter a char value:\n");
 	scanf("%c", &chrval);
