4ba369b3d3d54c8aeca5b454832010bbbc3e5986
huanqing19901001@163.com <huanqing19901001@163.com>
2024-03-15 20:07:32 +0800

    Update excercise01.

--- chapter04/programmingexcercise/excercise01_frst_lst_name.c
+++ chapter04/programmingexcercise/excercise01_frst_lst_name.c
@@ -13,8 +13,8 @@ int main(void)
 	char lst_name[40];
 
 	printf("请输入您的名和姓：\n");
-	scanf("%s %s", &frst_name, &lst_name);
-	printf("您的名：%s，您的姓：%s\n", frst_name, lst_name);
+	scanf("%s %s", frst_name, lst_name);
+	printf("%s,%s\n", frst_name, lst_name);
 
 	return 0;
 }
