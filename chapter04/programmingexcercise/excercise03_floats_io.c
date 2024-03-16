071d060054479f24e609855e10343f58c2d7df2e
huanqing19901001@163.com <huanqing19901001@163.com>
2024-03-16 19:27:44 +0800

    Create excercise 3.

---
+++ chapter04/programmingexcercise/excercise03_floats_io.c
@@ -0,0 +1,19 @@
+/*
+ * Excercise 3
+ * scanf and printf float values
+ *
+ * Date: 2023-03-16
+ * Author: FanHuanQing
+ *
+ */
+#include <stdio.h>
+int main(void)
+{
+	float a;
+
+	printf("Please enter a float value:\n");
+	scanf("%f", a);
+	printf("The input is %2.f or %2.e.\n", a, a);
+
+	return 0;
+}
