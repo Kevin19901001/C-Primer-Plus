bdbe13f946cc806787ae96090f412987fc12732d
huanqing19901001@163.com <huanqing19901001@163.com>
2024-03-19 22:20:43 +0800

    Finish exercise 4.

--- chapter04/programmingexcercise/exercise04_height.c
+++ chapter04/programmingexcercise/exercise04_height.c
@@ -8,5 +8,15 @@
 #include <stdio.h>
 int main(void)
 {
-  return 0;
+	int height;
+	char name[40];
+
+	printf("Please enter your height in centimeter and name:\n");
+	scanf("%d %s", &height, &name);
+
+	float height_in_feet = height / 100.0;
+	
+	printf("%s, you are %1.2f meter tall\n", name, height_in_feet);
+
+	return 0;
 }
