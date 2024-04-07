22e23c4aebfb4f5e5934fb93e27310efc393ad44
huanqing19901001@163.com <huanqing19901001@163.com>
2024-04-07 20:29:22 +0800

    Add case 2.

--- chapter05/case02_shoes2.c
+++ chapter05/case02_shoes2.c
@@ -1,1 +1,31 @@
+/*
+ * shoes22.c -- 计算多个不同鞋码对应的脚长
+ *
+ * Date: 2024-04-07
+ * Author: FanHuanQing
+ *
+ */
+#include <stdio.h>
+#define ADJUST 7.31
+
+int main(void)
+{
+
+	const double SCALE = 0.333;
+	double shoe, foot;
+
+	printf("Shoe size (men's) foot length\n");
+	shoe = 3.0;
+	while (shoe < 18.5)
+	{
+		foot = SCALE * shoe + ADJUST;
+		printf("%10.1f %15.2f inches\n", shoe, foot);
+		shoe = shoe + 1.0;
+	}
+
+	printf("If the shoe fits, wear it.\n");
+
+	return 0;
+
+}
 
