5f9774f40dbaae42929d94a13aa34712e31c2825
huanqing19901001@163.com <huanqing19901001@163.com>
2024-04-10 22:32:59 +0800

    Update files.

--- chapter05/celsius2fahrenheit.c
+++ chapter05/celsius2fahrenheit.c
@@ -6,4 +6,23 @@
  *
  */
 #include <stdio.h>
-int main(void){}
+#define ADJUST 32.0
+
+int main(void)
+{
+
+	const double SCALE = 1.8;
+	double celsius, fahrenheit;
+
+	printf("Celsius equal to Fahrenheit:\n");
+	celsius = 0.0;
+	while(celsius <= 100.0){
+		fahrenheit = SCALE * celsius + ADJUST;
+		printf("%8.2f    %8.2f fahrenheit.\n", celsius, fahrenheit);
+		celsius = celsius + 1.0;
+	
+	}
+
+	return 0;
+
+}
