0d4692b20111443031b3ebdbc9e6368ecd37f338
huanqing19901001@163.com <huanqing19901001@163.com>
2024-04-11 22:12:13 +0800

    Update files.

--- chapter05/km2mile.c
+++ chapter05/km2mile.c
@@ -10,7 +10,17 @@
 int main(void)
 {
 
+	const double SCALE = 0.6214;
+	double km, mile;
 
+	printf("km equals to mils\n");
+	km = 0.0;
+	while (km <= 100)
+	{
+		mile = SCALE * km + ADJUST;
+		printf("%10.4f %15.4f miles\n", km, mile);
+		km = km + 1.0;
+	}
 
 	return 0;
 
