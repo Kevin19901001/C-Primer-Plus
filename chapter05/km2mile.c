89f8aa14d5bd8b4bf66d896c237767699299b128
huanqing19901001@163.com <huanqing19901001@163.com>
2024-04-11 21:59:12 +0800

    Update files.

--- chapter05/km2mile.c
+++ chapter05/km2mile.c
@@ -1,7 +1,18 @@
 /*
  * 使用循环，将公里数转换为对应的英里数
- *
+ * 
+ * Date: 2024-04-11
+ * Author: FanHuanQing
  *
  */
 #include <stdio.h>
-int main(void){}
+#define ADJUST 0
+int main(void)
+{
+
+
+
+	return 0;
+
+}
+
